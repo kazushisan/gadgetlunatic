e9a44691e0cfbd8ccc1246be8df61a2478b22db8
Kazushi Konosu <kazushi@konosu.me>
2020-05-05 02:34:58 +0900

    fix: allow cross origin access to webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,14 @@ module.exports = env => {
 			})
 		],
 		devServer: {
-			port: 1314
+			port: 1314,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods':
+					'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+				'Access-Control-Allow-Headers':
+					'X-Requested-With, content-type, Authorization'
+			}
 		},
 		module: {
 			rules: [
