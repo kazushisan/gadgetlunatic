462a3b1b41dcd6dbfff724ba32b16097fd2b8e53
Kazushi Konosu <kazushi@konosu.me>
2023-02-12 15:51:10 +0900

    feat: setup prettier

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-import remarkGfm from 'remark-gfm'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
-import remarkHeadings from '@vcarl/remark-headings'
-import remarkMdxHeadings from './lib/remarkMdxHeadings'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
+import remarkGfm from 'remark-gfm';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import remarkHeadings from '@vcarl/remark-headings';
+import remarkMdxHeadings from './lib/remarkMdxHeadings';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({
-		remarkPlugins: [
-			remarkHeadings,
-			remarkMdxHeadings,
-			remarkFrontmatter,
-			remarkMdxFrontmatter,
-			remarkGfm,
-		]
-	})],
-	build: {
-		manifest: true,
-	}
-})
+  plugins: [
+    react(),
+    mdx({
+      remarkPlugins: [
+        remarkHeadings,
+        remarkMdxHeadings,
+        remarkFrontmatter,
+        remarkMdxFrontmatter,
+        remarkGfm,
+      ],
+    }),
+  ],
+  build: {
+    manifest: true,
+  },
+});
