312288bd10cb02ca08bf1d3e11dfcee7ece8b247
Kazushi Konosu <kazushi@konosu.me>
2023-02-25 22:56:10 +0900

    fix: fix import for vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mdx from './lib/mdx';
+import mdx from './plugins/rollup-plugin-mdx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
